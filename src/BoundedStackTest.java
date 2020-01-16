914b0d7cd61d0d088df7143282f8374702a07de3
somecharm <somecharm@gmail.com>
2020-01-16 16:51:00 +0300

    BoundedStack add test

--- src/BoundedStackTest.java
+++ src/BoundedStackTest.java
@@ -67,10 +67,10 @@ public class BoundedStackTest {
         boundedStack.pop();
         boundedStack.pop();
         boundedStack.pop();
-        assertEquals(1,boundedStack.get_pop_status());
+        assertEquals(1, boundedStack.get_pop_status());
         assertEquals(1, boundedStack.size());
         boundedStack.pop();
-        assertEquals(1,boundedStack.get_pop_status());
+        assertEquals(1, boundedStack.get_pop_status());
         assertEquals(0, boundedStack.size());
     }
 
@@ -78,15 +78,30 @@ public class BoundedStackTest {
     public void push() {
         BoundedStack<Object> boundedStack = new BoundedStack<>();
         boundedStack.push(0);
-        assertEquals(1,boundedStack.get_push_status());
+        assertEquals(1, boundedStack.get_push_status());
         boundedStack.push(1);
-        assertEquals(1,boundedStack.get_push_status());
+        assertEquals(1, boundedStack.get_push_status());
         boundedStack.push(2);
-        assertEquals(1,boundedStack.get_push_status());
+        assertEquals(1, boundedStack.get_push_status());
         boundedStack.push(3);
-        assertEquals(1,boundedStack.get_push_status());
+        assertEquals(1, boundedStack.get_push_status());
         assertEquals(3, boundedStack.peek());
         assertEquals(4, boundedStack.size());
+    }
+
+    @Test
+    public void push32() {
+        BoundedStack<Object> boundedStack = new BoundedStack<>();
+        for (int i = 1; i <33;i++){
+            boundedStack.push(i);
+            assertEquals(1, boundedStack.get_push_status());
+            assertEquals(i, boundedStack.peek());
+        }
+        assertEquals(32, boundedStack.size());
+        boundedStack.push(33);
+        assertEquals(2, boundedStack.get_push_status());
+        assertEquals(32, boundedStack.peek());
+        assertEquals(32, boundedStack.size());
     }
 
     @Test
@@ -98,37 +113,35 @@ public class BoundedStackTest {
         boundedStack.push(3);
         assertEquals(4, boundedStack.size());
         boundedStack.clear();
-        assertEquals(0,boundedStack.size());
-        assertEquals(0,boundedStack.get_push_status());
-        assertEquals(0,boundedStack.get_peek_status());
-        assertEquals(0,boundedStack.get_pop_status());
-        assertEquals(32, boundedStack);
-        assertEquals(32, boundedStack);
+        assertEquals(0, boundedStack.size());
+        assertEquals(0, boundedStack.get_push_status());
+        assertEquals(0, boundedStack.get_peek_status());
+        assertEquals(0, boundedStack.get_pop_status());
     }
 
     @Test
-    public void constructor(){
+    public void constructor() {
         BoundedStack<String> boundedStack = new BoundedStack<>(3);
-        assertEquals(0,boundedStack.get_push_status());
-        assertEquals(0,boundedStack.get_peek_status());
-        assertEquals(0,boundedStack.get_pop_status());
+        assertEquals(0, boundedStack.get_push_status());
+        assertEquals(0, boundedStack.get_peek_status());
+        assertEquals(0, boundedStack.get_pop_status());
         boundedStack.push("All ");
-        assertEquals(1,boundedStack.get_push_status());
-        assertEquals(0,boundedStack.get_peek_status());
-        assertEquals(0,boundedStack.get_pop_status());
+        assertEquals(1, boundedStack.get_push_status());
+        assertEquals(0, boundedStack.get_peek_status());
+        assertEquals(0, boundedStack.get_pop_status());
         boundedStack.push("is ");
-        assertEquals(1,boundedStack.get_push_status());
-        assertEquals(0,boundedStack.get_peek_status());
-        assertEquals(0,boundedStack.get_pop_status());
+        assertEquals(1, boundedStack.get_push_status());
+        assertEquals(0, boundedStack.get_peek_status());
+        assertEquals(0, boundedStack.get_pop_status());
         boundedStack.push("correct");
-        assertEquals(1,boundedStack.get_push_status());
-        assertEquals(0,boundedStack.get_peek_status());
-        assertEquals(0,boundedStack.get_pop_status());
-        assertEquals(3,boundedStack.size());
+        assertEquals(1, boundedStack.get_push_status());
+        assertEquals(0, boundedStack.get_peek_status());
+        assertEquals(0, boundedStack.get_pop_status());
+        assertEquals(3, boundedStack.size());
         boundedStack.push("out of bounds");
-        assertEquals(2,boundedStack.get_push_status());
-        assertEquals(0,boundedStack.get_peek_status());
-        assertEquals(0,boundedStack.get_pop_status());
-        assertEquals(3,boundedStack.size());
+        assertEquals(2, boundedStack.get_push_status());
+        assertEquals(0, boundedStack.get_peek_status());
+        assertEquals(0, boundedStack.get_pop_status());
+        assertEquals(3, boundedStack.size());
     }
 }
