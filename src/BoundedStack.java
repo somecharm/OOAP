709f8040ff11a24b7ee8be83662cdff5bf95b419
somecharm <somecharm@gmail.com>
2020-01-17 13:32:29 +0300

    BoundedStack fix abstract class

--- src/BoundedStack.java
+++ src/BoundedStack.java
@@ -1,15 +1,6 @@
-
 /*
-public class BoundedStack<T> {
-    // скрытые поля
-    private List<T> boundedStack; // основное хранилище ограниченного стека (далее - стек)
-    private int bounds = 32; // максимально допустимое количество элементов стека
-    private int peek_status; // статус запроса peek()
-    private int pop_status; // статус команды pop()
-    private int push_status; // статус команды push()
+abstract class BoundedStack<T>
 
-
-    // интерфейс класса, реализующий АТД BoundedStack
     public const int POP_NIL = 0;
     public const int POP_OK = 1;
     public const int POP_ERR = 2;
@@ -20,70 +11,41 @@ public class BoundedStack<T> {
     public const int PUSH_OK = 1;
     public const int PUSH_ERR = 2;
 
-
     // конструктор
-    // предусловие: получает целое положительное значение, задающее максимально допустимое количество элементов в стеке.
-    public BoundedStack<T> BoundedStack(int value) // постусловие: создан новый пустой стек
-        clear()
-        if value != null
-            bounds = value
+    // постусловие: создан новый пустой стек; максимально допустимое количество элементов равно заданному или равно 32.
+    public BoundedStack<T> BoundedStack(int value)
 
+    // команды
+    // предусловие: размер стека меньше максимально допустимого количества элементов
     // постусловие: в стек добавлено новое значение
     public void push(T value)
-        if bounds > size()
-            boundedStack.Append(value)
-            push_status = PUSH_OK
-        else
-            push_status = PUSH_ERR
 
     // предусловие: стек не пустой;
     // постусловие: из стека удалён верхний элемент
     public void pop()
-        if size() > 0
-            boundedStack.RemoveAt(-1)
-            peek_status = POP_OK
-        else
-            peek_status = POP_ERR
 
     // постусловие: из стека удалятся все значения
     public void clear()
-        boundedStack = [ ] // пустой список/стек
 
-        // начальные статусы для предусловий peek() и pop()
-        peek_status = PEEK_NIL
-        pop_status = POP_NIL
-        push_status = PUSH_NIL
-
-
+    // запросы
     // предусловие: стек не пустой
     public T peek()
-        if size() > 0
-            result = stack[-1]
-            peek_status = PEEK_OK
-        else
-            result = 0
-            peek_status = PEEK_ERR
-        return result
 
     public int size()
-        return stack.Length() // размер текущего стека
 
     // запросы статусов
     public int get_pop_status()
-        return pop_status
-
     public int get_peek_status()
-        return peek_status
+    public int get_push_status()
 
-     public int get_push_status()
-        return push_status;
-}
 */
 
 
 import java.util.LinkedList;
 
 public class BoundedStack<T> {
+
+    // скрытые поля
     private LinkedList<T> boundedStack;
     private int bounds = 32;
     private int peek_status;
--- src/BoundedStackTest.java
+++ src/BoundedStackTest.java
@@ -1,7 +1,4 @@
 import org.junit.Test;
-
-import java.util.Arrays;
-
 import static org.junit.Assert.*;
 
 public class BoundedStackTest {
